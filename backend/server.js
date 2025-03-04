ed89590aa2bae0fe631a844e9963878b7f49ad10
Rakshit Patil <patilrakshit388@gmail.com>
2025-03-04 16:28:33 +0530

    Cors options for server

--- backend/server.js
+++ backend/server.js
@@ -121,7 +121,9 @@ if (cluster.isMaster) {
           'http://192.168.1.95:5000',
           'http://192.168.1.71:5000',
           'http://192.168.1.151:3000',
-          'http://192.168.1.151:5000'
+          'http://192.168.1.151:5000',
+          'http://192.168.1.64:3000',
+          'http://192.168.1.64:5000'
         ],
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
