2645cddd89eaf064be3c41bedcf1e65a7ca5bc95
Rakshit Patil <patilrakshit388@gmail.com>
2025-03-07 11:12:53 +0530

    did minor change with header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,15 @@ import { Box, Typography } from '@mui/material';
 
 const Header = () => {
   return (
-    <Box display="flex" justifyContent="flex-start" alignItems="center" mb={4}>
+    <Box display="flex" justifyContent="flex-start" alignItems="center" mb={0}>
       <Typography 
         variant="h3" 
         sx={{ 
           fontWeight: 700,
           color: '#1976d2',
           letterSpacing: '0.5px',
-          textShadow: '1px 1px 2px rgba(0,0,0,0.1)'
+          textShadow: '1px 1px 2px rgba(0,0,0,0.1)',
+          marginTop: '12px'
         }}
       >
         Security Dashboard
